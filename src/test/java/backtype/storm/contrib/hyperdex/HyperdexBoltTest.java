674aea457ba6526c925f22f4e952087d4e0281d7
Chet Mancini <chetmancini@gmail.com>
2013-04-29 18:16:40 -0400

    CM: Add empty unit tests

---
+++ src/test/java/backtype/storm/contrib/hyperdex/HyperdexBoltTest.java
@@ -0,0 +1,15 @@
+package backtype.storm.contrib.hyperdex;
+
+public class HyperdexBoltTest {
+    public void testExecute() throws Exception {
+
+    }
+
+    public void testPrepare() throws Exception {
+
+    }
+
+    public void testDeclareOutputFields() throws Exception {
+
+    }
+}
---
+++ src/test/java/backtype/storm/contrib/hyperdex/HyperdexSpoutTest.java
@@ -0,0 +1,15 @@
+package backtype.storm.contrib.hyperdex;
+
+public class HyperdexSpoutTest {
+    public void testOpen() throws Exception {
+
+    }
+
+    public void testNextTuple() throws Exception {
+
+    }
+
+    public void testDeclareOutputFields() throws Exception {
+
+    }
+}
