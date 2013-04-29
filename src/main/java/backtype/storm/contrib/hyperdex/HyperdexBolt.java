3acc8ee710f88281f14dfa002082d267297abb2e
Chet Mancini <chetmancini@gmail.com>
2013-04-29 18:17:05 -0400

    CM: Fix compile errors in spout and bolt. implement soon

--- src/main/java/backtype/storm/contrib/hyperdex/HyperdexBolt.java
+++ src/main/java/backtype/storm/contrib/hyperdex/HyperdexBolt.java
@@ -1,13 +1,14 @@
 package backtype.storm.contrib.hyperdex;
 
-import backtype.storm.topology.base.BaseRichBolt;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class HyperdexBolt extends BaseRichBolt {
     private static Logger LOGGER = LoggerFactory.getLogger(HyperdexBolt.class);
@@ -22,5 +23,10 @@ public class HyperdexBolt extends BaseRichBolt {
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         LOGGER.debug("Connecting to Hyperdex");
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        throw new UnsupportedOperationException();
     }
 }
--- src/main/java/backtype/storm/contrib/hyperdex/HyperdexSpout.java
+++ src/main/java/backtype/storm/contrib/hyperdex/HyperdexSpout.java
@@ -1,10 +1,13 @@
 package backtype.storm.contrib.hyperdex;
 
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 
 @SuppressWarnings("serial")
@@ -14,11 +17,18 @@ public class HyperdexSpout extends BaseRichSpout {
 
     private SpoutOutputCollector spoutOutputCollector;
 
+    @Override
     public void open(Map conf, TopologyContext context, SpoutOutputCollector spoutOutputCollector) {
         this.spoutOutputCollector = spoutOutputCollector;
     }
 
+    @Override
     public void nextTuple() {
-    
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        throw new UnsupportedOperationException();
     }
 }
